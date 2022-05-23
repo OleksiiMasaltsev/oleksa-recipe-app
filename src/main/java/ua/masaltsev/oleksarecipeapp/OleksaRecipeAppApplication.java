53ed06ee1dc4ea996ecb1879946e9f789b18edb1
Masaltsev <a.o.masaltsev@gmail.com>
2022-05-23 21:32:40 +0300

    init

---
+++ src/main/java/ua/masaltsev/oleksarecipeapp/OleksaRecipeAppApplication.java
@@ -0,0 +1,13 @@
+package ua.masaltsev.oleksarecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OleksaRecipeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OleksaRecipeAppApplication.class, args);
+	}
+
+}
