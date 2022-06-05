ab36d54e4761adb8ac97fa6e1b9e4b3d4ac87b10
Masaltsev <a.o.masaltsev@gmail.com>
2022-06-05 23:17:25 +0300

    Controller MockMvc test

--- src/test/java/ua/masaltsev/oleksarecipeapp/controllers/IndexControllerTest.java
+++ src/test/java/ua/masaltsev/oleksarecipeapp/controllers/IndexControllerTest.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 import ua.masaltsev.oleksarecipeapp.service.RecipeService;
 
@@ -28,10 +32,31 @@ class IndexControllerTest {
 
     @Test
     void getIndex() {
+
+//        List<Recipe> recipes = new ArrayList<>();
+//        recipes.add(new Recipe());
+//        recipes.add(new Recipe());
+//
+//        when(recipeService.findAll()).thenReturn(recipes);
+
         String viewName = indexController.getIndex(model);
         assertEquals("index", viewName);
 
         verify(recipeService, times(1)).findAll();
         verify(model, times(1)).addAttribute(eq("recipes"), anyList());
     }
+
+    @Test
+    void testMockMvc() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("index"));
+    }
+
+
+
+
+
+
 }
