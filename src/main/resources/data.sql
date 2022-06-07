54cc3b1ed59d3c73167970745a810f967a927ed7
Masaltsev <a.o.masaltsev@gmail.com>
2022-06-08 01:16:39 +0300

    bootstrap added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,11 +2,12 @@ INSERT INTO category (description) VALUES ('American');
 INSERT INTO category (description) VALUES ('Italian');
 INSERT INTO category (description) VALUES ('Mexican');
 INSERT INTO category (description) VALUES ('Fast Food');
+
 INSERT INTO unit_of_measure (description) VALUES ('Teaspoon');
 INSERT INTO unit_of_measure (description) VALUES ('Tablespoon');
 INSERT INTO unit_of_measure (description) VALUES ('Cup');
 INSERT INTO unit_of_measure (description) VALUES ('Pinch');
 INSERT INTO unit_of_measure (description) VALUES ('Ounce');
-INSERT INTO recipe (description) VALUES ('Chicken soup');
-INSERT INTO recipe (description) VALUES ('Pasta');
-INSERT INTO recipe (description) VALUES ('Borsch');
+INSERT INTO unit_of_measure (description) VALUES ('Each');
+INSERT INTO unit_of_measure (description) VALUES ('Dash');
+INSERT INTO unit_of_measure (description) VALUES ('Pint');
